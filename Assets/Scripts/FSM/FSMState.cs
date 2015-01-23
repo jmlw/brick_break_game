caa604bedb6a073d9dda69b94327ebaa8af104c7
Josh Wood <josh.m.l.wood@gmail.com>
2015-01-22 23:39:39 -0600

    Updated state machine

---
+++ Assets/Scripts/FSM/FSMState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+abstract public class FSMState <T> {
+
+	
+    abstract public void StateEnter (T entity);
+	
+    abstract public void StateUpdate (T entity);
+
+    abstract public void StateExit (T entity);
+}
---
+++ Assets/Scripts/FSM/FSMachine.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FSMachine <T> {
+
+    private T Owner;
+    private FSMState<T> CurrentState;
+    private FSMState<T> PreviousState;
+    private FSMState<T> GlobalState;
+    
+    public void Awake() {
+        CurrentState = null;
+        PreviousState = null;
+        GlobalState = null;
+    }
+    
+    public void Configure(T owner, FSMState<T> InitialState) {
+        Owner = owner;
+        ChangeState(InitialState);
+    }
+    
+    public void  Update() {
+        if (GlobalState != null)  GlobalState.StateUpdate(Owner);
+        if (CurrentState != null) CurrentState.StateUpdate(Owner);
+    }
+    
+    public void  ChangeState(FSMState<T> NewState) {
+        PreviousState = CurrentState;
+        if (CurrentState != null)
+            CurrentState.StateExit(Owner);
+        CurrentState = NewState;
+        if (CurrentState != null)
+            CurrentState.StateEnter(Owner);
+    }
+    
+    public void  RevertToPreviousState() {
+        if (PreviousState != null)
+            ChangeState(PreviousState);
+    }
+};
+}
