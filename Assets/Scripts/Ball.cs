05283ee1069da30251a1d6df22353aaf502b3a5f
Josh Wood <josh.m.l.wood@gmail.com>
2015-07-02 13:58:26 -0500

    removing some old items updates to ball prefab

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -13,26 +13,9 @@ public class Ball : MonoBehaviour {
         data.registerBall(this);
     }
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
     void OnDestroy() {
         data.removeBall(this);  
-    }
-
-    void FixedUpdate() {
-        velocityCheck();
-    }
-
-    private void velocityCheck() {
-
     }
 
     public void launch() {
